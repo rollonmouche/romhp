49f8fa61813b9f33db5eb13ff181019383d1525b
tw <wendt.t@gmx.de>
2020-04-19 14:21:13 +0200

    Global constants in make.py

--- make.py
+++ make.py
@@ -3,6 +3,28 @@ defined markers with defined html code. In a created file 'child.html', the
 included html code is taken from 'child_content.html'.
 """
 
+BASE_FILE = 'index.html'
+PAGES = [
+    'live',
+    'media',
+    'booking',
+    'imprint',
+]
+INCLUDEMARKER_BEGIN = '<!-- #include-html-begin -->'
+INCLUDEMARKER_END = '<!-- #include-html-end -->'
+PAGEFILE_PATTERN = '{}.html'  # format with PAGES[n]
+INCLUDEFILE_PATTERN = '{}_content.html'  # format with PAGES[n]
+
+ACTIVE_CLASS = 'active'
+NOACTIVE_CLASS = 'noactive'
+
+GIGFILE_UPCOMING = 'gigs_upcoming.txt'
+GIGFILE_DONE = 'gigs_done.txt'
+UPCOMING_TITLE = 'Upcoming'
+DONE_TITLE = 'Done'
+NOGIGS = 'No upcoming shows'
+GIG_TABLE_HTML_FILE = INCLUDEFILE_PATTERN.format('live')
+
 
 def get_file_content(filename):
     with open(filename, 'r') as file:
@@ -10,8 +32,15 @@ def get_file_content(filename):
     return content
 
 
-def make_pages(pages, source_filename, begin_marker, end_marker):
-    source_data = get_file_content(source_filename)
+def make_pages(
+    pages=PAGES,
+    basefile=BASE_FILE,
+    begin_marker=INCLUDEMARKER_BEGIN,
+    end_marker=INCLUDEMARKER_END,
+    pagefile_pattern=PAGEFILE_PATTERN,
+    includefile_pattern=INCLUDEFILE_PATTERN,
+):
+    source_data = get_file_content(basefile)
     idx_begin = source_data.find(begin_marker)
     idx_end = source_data.find(end_marker, idx_begin) + len(end_marker)
     indent_len = idx_begin - source_data.rfind('\n', 0, idx_begin) - 1
@@ -19,29 +48,34 @@ def make_pages(pages, source_filename, begin_marker, end_marker):
     text_to_replace = source_data[idx_begin:idx_end]
     print('make pages …')
     for page in pages:
-        output_filename = page + '.html'
+        output_filename = pagefile_pattern.format(page)
         with open(output_filename, 'w') as outfile:
             print(output_filename)
             outfile.write('<!-- Automatically generated file by make.py -->\n')
-            content = get_file_content(page + '_content.html')
+            content = get_file_content(includefile_pattern.format(page))
             content = content.replace('\n', '\n' + indent)
             outdata = source_data.replace(text_to_replace, content)
             outfile.write(outdata)
         
 
-def set_active_class(pages):
-    """Set "active" and "noactive" classes in all files."""
+def set_active_class(
+    pages=PAGES,
+    active_class=ACTIVE_CLASS,
+    noactive_class=NOACTIVE_CLASS,
+    pagefile_pattern=PAGEFILE_PATTERN,
+):
+    """Set active and noactive classes in all files."""
     print('set active class …')
     for page in pages:
-        filename = page + '.html'
+        filename = pagefile_pattern.format(page)
         print(filename)
         lines = get_file_content(filename).split('\n')
         with open(filename, 'w') as file:
             for line in lines:
-                if 'noactive' in line and page in line:
-                    line = line.replace('noactive', 'active')
-                elif 'active' in line and 'index' in line:
-                    line = line.replace('active', 'noactive')
+                if noactive_class in line and page in line:
+                    line = line.replace(noactive_class, active_class)
+                elif active_class in line and 'index' in line:
+                    line = line.replace(active_class, noactive_class)
                 file.write(line + '\n')
 
 
@@ -122,7 +156,7 @@ def gig2html(gig, set_hyperlink=True):
     return '\n'.join(html_lines)
 
 
-def create_gig_table(gigfile, title, set_hyperlink=True):
+def create_gig_table(gigfile, title, set_hyperlink, alt=NOGIGS):
     keys = ['date', 'venue', 'url', 'add']
     gigs = load_gigs(gigfile, keys)
     if gigs:
@@ -130,7 +164,7 @@ def create_gig_table(gigfile, title, set_hyperlink=True):
         for gig in gigs:
             gigs_html += [gig2html(gig, set_hyperlink=set_hyperlink)]
     else:
-        gigs_html = ['<tr><td style="text-align: center">No upcoming shows</td></tr>']
+        gigs_html = ['<tr><td style="text-align: center">{}</td></tr>'.format(alt)]
     
     html_lines = [
         '<table role="table" aria-label="{}">'.format(title),
@@ -157,29 +191,19 @@ def write_gig_tables_html(tables, outfile):
 
 def main():
     upcoming_table = create_gig_table(
-        gigfile='gigs_upcoming.txt',
-        title='Upcoming',
+        gigfile=GIGFILE_UPCOMING,
+        title=UPCOMING_TITLE,
         set_hyperlink=True,
     )
     done_table = create_gig_table(
-        gigfile='gigs_done.txt',
-        title='Done',
+        gigfile=GIGFILE_DONE,
+        title=DONE_TITLE,
         set_hyperlink=False,
     )
-    outfile = 'live_content.html'
-    write_gig_tables_html([upcoming_table, done_table], outfile)
+    write_gig_tables_html([upcoming_table, done_table], GIG_TABLE_HTML_FILE)
 
-    pages = [
-        'live',
-        'media',
-        'booking',
-        'imprint',
-    ]
-    source_filename = 'index.html'
-    begin_marker = '<!-- #include-html-begin -->'
-    end_marker = '<!-- #include-html-end -->'
-    make_pages(pages, source_filename, begin_marker, end_marker)
-    set_active_class(pages)
+    make_pages()
+    set_active_class()
 
 
 if __name__ == '__main__':
